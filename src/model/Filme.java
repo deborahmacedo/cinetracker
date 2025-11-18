36612693495f3acd6067610d49a670c0a94d3b7b
deborahmacedo <deborah.macedo.104@ufrn.edu.br>
2025-11-18 15:21:22 -0300

    getters e setters

--- src/model/Filme.java
+++ src/model/Filme.java
@@ -1,6 +1,23 @@
 public class Filme {
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     String titulo;
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
     double nota;
 
     public Enum status {
