292977cefea9487b968f8b0cec655d4efda0d9d5
Júlia Santos <julia.lima.713@ufrn.edu.br>
2025-11-23 14:50:41 -0300

    Correção no Enum StatusFilme

--- src/model/StatusFilme.java
+++ src/model/StatusFilme.java
@@ -1,17 +1,6 @@
-package model;
-
 public class StatusFilme {
     public Enum status {
         ASSISTIDO,
-        PARA_ASSISTIR;
-    }
-
-    public Enum getStatus() {
-        return status;
-    }
-
-    public void setStatus(Enum status) {
-        this.status = status;
+                PARA_ASSISTIR;
     }
 }
-
